81a972dd6325536590c4b295117ea18d84b5addd
colten krelo <coltenkrelo@coltens-MBP.attlocal.net>
2024-09-19 16:33:45 -0500

    second test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,3 +5,19 @@ describe('Simple Cypress Test', () => {
     cy.get('#greeting').should('contain', 'Hello, World!');
   });
 });
+
+describe('Navigation Test', () => {
+  it('Should navigate to the About Us page when clicking the About link', () => {
+    // Visit the homepage
+    cy.visit('http://localhost:8080');
+
+    // Click on the About Us link
+    cy.get('#about-link').click();
+
+    // Verify that the new page contains the correct content
+    cy.url().should('include', '/about.html');
+    cy.contains('h1', 'About Our Website');
+    cy.contains('p', 'Welcome to the about us page.');
+  });
+});
+
